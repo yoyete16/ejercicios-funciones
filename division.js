00dbb9a94e21ab4f874b9b2a1c26d4754e07101b
yoyete16 <91971870+yoyete16@users.noreply.github.com>
2023-03-20 11:11:26 +0100

    Add files via upload

---
+++ division.js
@@ -0,0 +1,11 @@
+function division(num1,num2){
+resultado = num1/num2;
+if (num2 = 0){
+return "no se puede realizar la division";
+}else{
+    return resultado;
+}
+}
+
+let res = division(1,2);
+console.log("el cociente es "  + resultado);
---
+++ maximo.js
@@ -0,0 +1,13 @@
+function obtenerMayor(numero1, numero2) {
+    if (numero1 > numero2) {
+      return numero1;
+    } else {
+      return numero2;
+  
+    }
+  
+}
+
+
+  let resu = obtenerMayor(7,5);
+  console.log(" el numero mas grande es" +  resultado);
