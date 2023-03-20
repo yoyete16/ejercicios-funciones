8a0b3814a20bbd039417508ee6820e71c0bbf1d2
yoyete16 <91971870+yoyete16@users.noreply.github.com>
2023-03-20 10:40:16 +0100

    Update producto.js

--- producto.js
+++ producto.js
@@ -1,5 +1,11 @@
+//definir funcion
 function producto(numero1,numero2){
-  
-  
-return numero1*numero2
+
+return numero1*numero2;
   } 
+  
+  //llamada
+  let resultado = producto(3  ,3);
+
+  //msotrar
+  console.log("el producto de esos numeros es",resultado);
