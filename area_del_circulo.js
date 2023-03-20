4c25185a0c318a6fbe8f1bc580fb68ec9733a3fd
yoyete16 <91971870+yoyete16@users.noreply.github.com>
2023-03-20 10:55:40 +0100

    Add files via upload

---
+++ area_del_circulo.js
@@ -0,0 +1,9 @@
+function area_circulo(radio){
+    π =3.14159265359;
+    let resultado =radio*2*π;
+ return resultado;
+}
+
+let result = area_circulo(3);
+
+console.log("el area es " +  resultado );
